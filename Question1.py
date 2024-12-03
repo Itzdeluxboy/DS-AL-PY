51efa356f4fe0ae4897717ddf95d869089ad70e8
Ruben <59733440+Itzdeluxboy@users.noreply.github.com>
2024-12-03 23:03:02 +0530

    Ex_Output2
    If the given_number is in the middle of the list 'cards'

--- Question1.py
+++ Question1.py
@@ -1,14 +1,16 @@
 def find_given_number(cards, given_number):
     pass
 
-
-cards = [13,12,11,7,4,1,0]
-given_number = 7
-output = 3
-
-result = find_given_number(cards, given_number)
+test = {'input':{'cards': [13,12,11,7,4,1,0], 'given_number': 7}, 'output' : 3}
+result = find_given_number(**test['input']) == test['output']
 print(result)
-if result==output:
+
+
+if result == test['output']:
     print(True)
 else:
     print(False)
+# cards = [13,12,11,7,4,1,0]
+# given_number = 7
+# output = 3
+

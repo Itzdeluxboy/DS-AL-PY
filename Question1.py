83dab5e16023671ca2a587f2799d4db3b83ddbb8
Ruben <59733440+Itzdeluxboy@users.noreply.github.com>
2024-12-03 22:37:35 +0530

    Ex_Output_1
    Example Output 1 w/out the function to check whether if the given_number is found out or not

--- Question1.py
+++ Question1.py
@@ -1,2 +1,14 @@
 def find_given_number(cards, given_number):
     pass
+
+
+cards = [13,12,11,7,4,1,0]
+given_number = 7
+output = 3
+
+result = find_given_number(cards, given_number)
+print(result)
+if result==output:
+    print(True)
+else:
+    print(False)

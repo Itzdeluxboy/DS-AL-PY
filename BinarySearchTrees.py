594e52b7d1f7c963c4503daaaabe1ae196a3e2df
Ruben <59733440+Itzdeluxboy@users.noreply.github.com>
2024-12-06 23:37:16 +0530

    Added new class and UserDatabase functions
    Add the class: UserDatabase and Added the Insert, find, update, list functions

--- BinarySearchTrees.py
+++ BinarySearchTrees.py
@@ -8,8 +8,28 @@ class User:
     def introduce_yourself(self, cont_name):
         print("Hi {}, I'm {}! Contact me at {}".format(cont_name, self.name, self.email))
 
+
+    def __repr__(self):
+        return "User(username='{}', name='{}', email='{}')".format(self.username, self.name, self.email)
+
+    def __str__(self):
+        return self.__repr__()
+
 user = User('john', 'John Doe', 'john@doe.com')
-print(user.name, user.username, user.email)
 
 user2= User('jane' , "Jane Doe", "jane@doe.com")
-User.introduce_yourself(user2,'David')
+
+print(user,user2)
+
+
+
+class UserDatabase:
+    def insert(self,user):
+        pass
+    def find(self, username):
+        pass
+    def update(self,user):
+        pass
+    def list_all(self):
+        pass
+    

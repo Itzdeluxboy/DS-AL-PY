a63d019ff04cfa9a4635a94d86514d6713fe8247
Ruben <59733440+Itzdeluxboy@users.noreply.github.com>
2024-12-10 00:02:43 +0530

    New Function Added
    A new function was implemented to insert value into the BST

--- Binary Tree.py
+++ Binary Tree.py
@@ -120,5 +120,21 @@ sonaksh = User('sonaksh', 'Sonaksh Kumar', 'sonaksh@example.com')
 vishal = User('vishal', 'Vishal Goel', 'vishal@example.com')
 
 
-tree = BTSNode(jadhesh.username, jadhesh)
-print(tree.key, tree.value)        
+def insert(node, key, value):
+    if node is None:
+        node = BTSNode(key, value)
+    elif key < node.key:
+        node.left = insert(node.left, key, value)
+        node.left.parent = node
+    elif key > node.key:
+        node.right = insert(node.right, key, value)
+        node.right.parent = node
+    return  
+
+tree = insert(None, jadhesh.username, jadhesh)
+insert(tree, biraj.username, biraj)
+insert(tree, sonaksh.username, sonaksh)
+insert(tree, aakash.username, aakash)
+insert(tree, hemanth.username, hemanth)
+insert(tree, siddhant.username, siddhant)
+insert(tree, vishal.username, siddhant)
